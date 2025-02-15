31bed274930bd6398104f1c34eeb1feeaa44ef93
parthgoyal13 <nparthgoyal.skncoe.comp@gmail.com>
2025-02-15 18:51:15 +0530

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ initializeDatabase();
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
 async function readAllHotels(hotels) {
   try {
